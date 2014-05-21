30702eb5e61d6f992cd397b0701238995b0d4719
mukundp <mukundpotdar@alohatechnology.com>
2014-05-21 14:23:43 +0530

    # Registered UPS New Rating API (UPS2) for customer Sprint.

---
+++ sql/arrs_2014-05-20-MP-1.sql
@@ -0,0 +1,12 @@
+/* Register UPS New Rating API (UPS2) for customer Sprint */
+
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0000';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0001';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0002';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0003';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0004';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0005';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0006';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0007';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0008';
+UPDATE customerservice set ratetypeid='UPS2RATINGAPI' where customerserviceid='SPRINTUPS0009';
