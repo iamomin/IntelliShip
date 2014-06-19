5985bd8d73ef4e41d7fe6c05696bbfb22e786199
imranm <imranm@alohatechnology.com>
2014-06-19 16:41:56 +0530

    Added IE specific error fix getComputedStyle for html2canvas

--- root/static/js/html2canvas/html2canvas.js
+++ root/static/js/html2canvas/html2canvas.js
@@ -57,6 +57,22 @@ _html2canvas.Util.asFloat = function(v) {
   };
 })();
 
+if (!window.getComputedStyle) {
+	window.getComputedStyle = function(el, pseudo) {
+		this.el = el;
+		this.getPropertyValue = function(prop) {
+			var re = /(\-([a-z]){1})/g;
+			if (prop == 'float') prop = 'styleFloat';
+			if (re.test(prop)) {
+				prop = prop.replace(re, function () {
+					return arguments[2].toUpperCase();
+				});
+			}
+			return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+		}
+		return this;
+	}
+}
 
 _html2canvas.Util.parseBackgroundImage = function (value) {
     var whitespace = ' \r\n\t',
