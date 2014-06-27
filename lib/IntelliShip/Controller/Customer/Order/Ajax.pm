b7a293a8b946c40e13d6bb4cc3d784d7757f06b2
Rohit Jain <mukundpotdar@alohatechnology.com>
2014-06-28 00:06:07 +0530

    edispatch request/cancel functionality changes

--- lib/IntelliShip/Controller/Customer/Order/Ajax.pm
+++ lib/IntelliShip/Controller/Customer/Order/Ajax.pm
@@ -839,6 +839,7 @@ sub mark_shipment_as_printed
 		if ($Shipment->has_pickup_request)
 			{
 			$self->send_pickup_request($Shipment);
+			$self->SendDispatchNotification('PICKUP');
 			}
 
 		$c->log->debug("... Marked shipment $shipmentid as 'Printed'");
