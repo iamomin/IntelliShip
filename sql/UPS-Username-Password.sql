5d6ca4e61f23c8a98ff2d8737abc9653ab61f131
Vinay Chaudhari <vinayc@alohatechnology.com>
2014-07-16 19:43:56 +0530

    Username and password for UPS2 Api.

---
+++ sql/UPS-Username-Password.sql
@@ -0,0 +1,118 @@
+ 
+insert into servicecsdata values ('SPRINTUPS0011',4,'SPRINTUPS0001',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0012',4,'SPRINTUPS0001',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0013',4,'SPRINTUPS0002',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0014',4,'SPRINTUPS0002',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0015',4,'SPRINTUPS0003',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0016',4,'SPRINTUPS0003',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0017',4,'SPRINTUPS0004',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0018',4,'SPRINTUPS0004',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0019',4,'SPRINTUPS0005',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('ups2dayair020',4,'SPRINTUPS0005',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0021',4,'SPRINTUPS0006',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0022',4,'SPRINTUPS0006',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0023',4,'SPRINTUPS0007',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0024',4,'SPRINTUPS0007',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0025',4,'SPRINTUPS0008',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0026',4,'SPRINTUPS0008',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0027',4,'SPRINTUPS0009',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0028',4,'SPRINTUPS0009',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SPRINTUPS0029',4,'SPRINTUPS0000',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SPRINTUPS0030',4,'SPRINTUPS0000',2,'upswbpw','2wsx!QAZ','now',null);
+
+
+
+insert into servicecsdata values ('SOURCE000010',4,'SOURCE000009',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000011',4,'SOURCE000009',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000012',4,'SOURCE000010',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000013',4,'SOURCE000010',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000014',4,'SOURCE000011',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000015',4,'SOURCE000011',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000016',4,'SOURCE000012',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000017',4,'SOURCE000012',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000018',4,'SOURCE000013',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000019',4,'SOURCE000013',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000020',4,'SOURCE000014',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000021',4,'SOURCE000014',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000022',4,'SOURCE000015',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000023',4,'SOURCE000015',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000024',4,'SOURCE000016',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000025',4,'SOURCE000016',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000026',4,'SOURCE000017',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000027',4,'SOURCE000017',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SOURCE000028',4,'SOURCE000018',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SOURCE000029',4,'SOURCE000018',2,'upswbpw','2wsx!QAZ','now',null);
+
+
+
+
+insert into servicecsdata values ('EFCUPS0000000',4,'EFCUPS0000002',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000001',4,'EFCUPS0000002',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000002',4,'EFCUPS0000003',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000003',4,'EFCUPS0000003',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000004',4,'EFCUPS0000004',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000005',4,'EFCUPS0000004',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000006',4,'EFCUPS0000005',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000007',4,'EFCUPS0000005',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000008',4,'EFCUPS0000006',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000009',4,'EFCUPS0000006',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000010',4,'EFCUPS0000007',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000011',4,'EFCUPS0000007',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000012',4,'EFCUPS0000008',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000013',4,'EFCUPS0000008',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000014',4,'EFCUPS0000009',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000015',4,'EFCUPS0000009',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000016',4,'EFCUPS0000010',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000017',4,'EFCUPS0000010',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000018',4,'EFCUPS0000011',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000019',4,'EFCUPS0000011',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000020',4,'EFCUPS0000012',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000021',4,'EFCUPS0000012',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('EFCUPS0000022',4,'EFCUPS0000013',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('EFCUPS0000023',4,'EFCUPS0000013',2,'upswbpw','2wsx!QAZ','now',null);
+
+
+
+insert into servicecsdata values ('SC2UPS0000000',4,'SC2UPS0000002',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SC2UPS0000001',4,'SC2UPS0000002',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SC2UPS0000002',4,'SC2UPS0000003',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SC2UPS0000003',4,'SC2UPS0000003',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SC2UPS0000004',4,'SC2UPS0000004',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SC2UPS0000005',4,'SC2UPS0000004',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SC2UPS0000006',4,'SC2UPS0000005',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SC2UPS0000007',4,'SC2UPS0000005',2,'upswbpw','2wsx!QAZ','now',null);
+
+insert into servicecsdata values ('SC2UPS0000008',4,'SC2UPS0000006',2,'upswsid','SprintAW5282','now',null);
+insert into servicecsdata values ('SC2UPS0000009',4,'SC2UPS0000006',2,'upswbpw','2wsx!QAZ','now',null);
